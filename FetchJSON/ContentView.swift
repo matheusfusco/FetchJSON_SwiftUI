67f6473d37d04b63ec79b3fc45928dcaeafc8efc
Matheus <matheusfusco@gmail.com>
2019-06-19 13:47:13 -0300

    Initial Commit

---
+++ FetchJSON/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FetchJSON
+//
+//  Created by Matheus Pacheco Fusco on 19/06/19.
+//  Copyright © 2019 Matheus Pacheco Fusco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
